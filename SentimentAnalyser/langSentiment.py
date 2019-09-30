ff9e0ea7ae478472d97c9acbed56911b6177e1e3
Ricardo Ortiz <ricortiz13@gmail.com>
2019-09-29 21:16:46 -0400

    Modified the language analyzer to include processing the the data in the database and push back the sentiment score into the database accordingly.

--- SentimentAnalyser/langSentiment.py
+++ SentimentAnalyser/langSentiment.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
+import os
+import pyodbc
+
 from google.cloud import language
 from google.cloud.language import enums
+from google.cloud.language import types
 
 
 class LanguageAnalyer(object):
@@ -37,12 +41,40 @@ class LanguageAnalyer(object):
         }
 
         return self.client.analyze_sentiment(
-            document, encoding_type=self.encode_type)
+            document, encoding_type=self.encode_type).document_sentiment
 
 
 def main():
-    tweet = LanguageAnalyer(lang="en")
-    tweet.analyze("I am very happy today!")
+    tweet = LanguageAnalyer()
+    
+    # Create connection to database
+    server = 'tcp:ec601database-server.database.windows.net, 1433'
+    database = 'ec601database'
+    username = os.getenv('DB_USER')
+    password = os.getenv('DB_PASSWD')
+
+    cnxn = pyodbc.connect('DRIVER={ODBC DRIVER 17 for SQL Server};\
+                           SERVER=' + server + ';\
+                           DATABASE=' + database + ';\
+                           UID=' + username + ';\
+                           PWD=' + password)
+    cursor = cnxn.cursor()
+
+    # Get the table data
+    cursor.execute('select * from test_defaultKeywordsSearchStorage')
+
+    # Update query
+    update_q = "UPDATE test_defaultKeywordsSearchStorage SET TweetsNLPScore = ? WHERE TweetsId = ?;"
+
+    # Iterate through rows
+    for row in cursor.fetchall():
+        # Get sentiment of individual tweet
+        sentiment = tweet.analyze(row.TweetsText)
+        cursor.execute(update_q, sentiment.score*10, row.TweetsId)
+    
+    # Commit changes to DB
+    cnxn.commit()
+    print("Database updated with score!")
 
 
 if __name__ == '__main__':
